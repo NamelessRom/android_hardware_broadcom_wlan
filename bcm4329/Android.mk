2c1bb6e6cf268e3462d071af2416f36ce811f46e
Alexander Martinz <eviscerationls@gmail.com>
2013-10-11 17:06:18 +0200

    Add removed sources from cm-10.1 to enable building for some devices, including p970
    Change-Id: I1af7698542766a4eb6b72e524345dd001e153f1a

--- bcm4329/Android.mk
+++ bcm4329/Android.mk
@@ -1,6 +1,8 @@
+ifeq ($(LEGACY_WLAN),true)
 ifeq ($(BOARD_WLAN_DEVICE),bcm4329)
     include $(call all-subdir-makefiles)
 endif
 ifeq ($(BOARD_WLAN_DEVICE),bcm4330)
     include $(call all-subdir-makefiles)
+endif
 endif
